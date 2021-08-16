76b07b8f5271bc64d25bec3c735ab260b716376f
Harsh V Singh <harshv.singh@outlook.com>
2021-08-16 17:35:08 +0530

    Logging and confirmation msgs for tennis booking

--- tennis-booking.py
+++ tennis-booking.py
@@ -1,6 +1,9 @@
 import sys
 import json
 import datetime
+import logging
+import yagmail
+import pywhatkit
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,34 +12,42 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-def readCredentialsJSON(file_name="credentials.json", key="apna-complex"):
+
+def readCredentialsJSON(key, file_name="credentials.json"):
     creds = json.load(open(file_name))
     return (creds[key])
 
+
 def bookTennisSlots(date_delta=1, slot_hour=None, court_num=None):
-    apna_complex_creds = readCredentialsJSON()
+    slot_booked = False
+    booking_stage = "Initializing ApnaComplex driver"
+    apna_complex_creds = readCredentialsJSON(key="apna-complex")
     driver = get_apnacomplex_driver(creds=apna_complex_creds)
-    delay = 5 #seconds
-    
+    delay = 60 #seconds
     try:
+        booking_stage = "Waiting for facilities list"
         facilities_table = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "facilities")))
         for row in facilities_table.find_elements_by_xpath(".//tbody//tr"):
             all_cells = row.find_elements_by_xpath(".//td")
-            slot_booked = False
+            booking_stage = "Searching for valid court"
             if is_valid_court(all_cells, court_num):
                 booking_links = all_cells[-1]
                 all_links = booking_links.find_elements_by_xpath(".//a")
+                booking_stage = "Iterating all facility links"
                 for booking_link in all_links:
                     image_title = booking_link.find_elements_by_xpath(".//img")[0].get_attribute("title")
+                    booking_stage = "Identifying booking link"
                     if image_title == "Make a booking for this facility":
                         booking_url = booking_link.get_attribute("href")
+                        booking_stage = "Initializing a new booking"
                         slot_booked = make_booking(creds=apna_complex_creds, booking_url=booking_url, \
                             date_delta=date_delta, slot_hour=slot_hour, delay=delay)
                         break
             if slot_booked:
+                booking_stage = "Booking completed successfully"
                 break
-    except TimeoutException:
-        print("Facilities page did not load correctly.")
+    except:
+        logging.error("Booking failed at stage: %s" % booking_stage)
     
     # time.sleep(delay)
     driver.quit()
@@ -62,6 +73,18 @@ def get_apnacomplex_driver(creds, url=None):
     return (driver)
 
 
+def get_booking_date(date_delta):
+    return ((datetime.date.today() + datetime.timedelta(days=date_delta)).strftime("%d/%m/%Y"))
+
+
+def get_booking_time_slot(slot_hour):
+    if slot_hour is None:
+            slot_hour = datetime.datetime.now().strftime("%H")
+    time_slot = str.zfill(str(slot_hour), 2)
+    time_slot  = "{}:00 - {}:45".format(time_slot, time_slot)
+    return (time_slot)
+
+
 def make_booking(creds, booking_url, date_delta, slot_hour, delay):
     slot_booked = False
     try:
@@ -71,14 +94,9 @@ def make_booking(creds, booking_url, date_delta, slot_hour, delay):
         instructions_checkbox.click()
         # Set booking date (today + 1)
         date_selector = driver.find_element(by=By.NAME, value="booking_date")
-        booking_date = (datetime.date.today() + datetime.timedelta(days=date_delta)).strftime("%d/%m/%Y")
-        date_selector.send_keys(booking_date)
+        date_selector.send_keys(get_booking_date(date_delta=date_delta))
         # Set time slot
-        if slot_hour is None:
-            slot_hour = datetime.datetime.now().strftime("%H")
-        time_slot = str.zfill(str(slot_hour), 2)
-        time_slot  = "{}:00 - {}:45".format(time_slot, time_slot)
-        slot_selector = driver.find_element_by_xpath("//select[@name='facility_time_slot_id']/option[text()='" + time_slot + "']")
+        slot_selector = driver.find_element_by_xpath("//select[@name='facility_time_slot_id']/option[text()='" + get_booking_time_slot(slot_hour=slot_hour) + "']")
         slot_selector.click()
         # Submit form
         submit_button = driver.find_element(by=By.NAME, value="make_booking")
@@ -88,19 +106,18 @@ def make_booking(creds, booking_url, date_delta, slot_hour, delay):
         confirm_button.click()
         # Verify confirmation message
         status_message = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "status_message")))
-        print(status_message.text)
         slot_booked = (status_message.text == "Booking completed successfully.")
        
     except TimeoutException:
-        print("Booking page did not load correctly.")
+        logging.error("Booking page did not load correctly.")
         slot_booked = False
     except:
-        print("Some error occured during booking.")
+        logging.error("Unknown error occured during booking.")
         slot_booked = False
     finally:
         driver.quit()
-    
     return (slot_booked)
+
 
 def is_valid_court(all_cells, court_num):
     facility_name = all_cells[0].text
@@ -108,12 +125,46 @@ def is_valid_court(all_cells, court_num):
     is_valid_court_num = (court_num is None) or facility_name.endswith(str(court_num))
     return (is_tennis_court and is_valid_court_num)
 
-if __name__ == "__main__":
+
+def send_status_email(msg_text):
+    gmail_creds = readCredentialsJSON(key="gmail")
+    yag = yagmail.SMTP(user=gmail_creds["id"], password=gmail_creds["password"])
+    yag.send(gmail_creds["id"], msg_text, msg_text)
+    
+
+def send_status_whatsapp(msg_text):
+    try:
+        whatsapp_creds = readCredentialsJSON(key="whatsapp")
+        msg_hour = datetime.datetime.now().hour
+        msg_min = datetime.datetime.now().minute + 1
+        logging.info("Sending confirmation msg to %s" % whatsapp_creds["mobile"])
+        pywhatkit.sendwhatmsg(whatsapp_creds["mobile"], msg_text, msg_hour, msg_min, tab_close=True)
+    except:
+        logging.error("Error sending whatsapp msg to %s" % whatsapp_creds["mobile"])
+
+
+def main():
+    logging.basicConfig(filename="tennis-booking.log", level=logging.INFO)
     try:
         date_delta = int(sys.argv[1]) if (len(sys.argv) > 1) else 1
         slot_hour = int(sys.argv[2]) if (len(sys.argv) > 2) else None
         court_num = int(sys.argv[3]) if (len(sys.argv) > 3) else None
     except:
-        print("Invalid arguments provided!")
+        logging.error("Invalid arguments provided!")
+    
+    logging.info("Initiating booking for %s at %s" %(get_booking_date(date_delta=date_delta), get_booking_time_slot(slot_hour=slot_hour)))
+    result = bookTennisSlots(date_delta=date_delta, slot_hour=slot_hour, court_num=court_num)
+    if result:
+        success_msg = "Booking successfully completed for %s at %s" %(get_booking_date(date_delta=date_delta), get_booking_time_slot(slot_hour=slot_hour))
+        logging.info(success_msg)
+        send_status_email(msg_text=success_msg)
+        send_status_whatsapp(msg_text=success_msg)
+    else:
+        # Send failure email 
+        failure_msg = "Booking unsuccessful for %s at %s" %(get_booking_date(date_delta=date_delta), get_booking_time_slot(slot_hour=slot_hour))
+        logging.warn(failure_msg)
+        send_status_email(msg_text=failure_msg)
+        send_status_whatsapp(msg_text=failure_msg)
 
-    bookTennisSlots(date_delta=date_delta, slot_hour=slot_hour, court_num=court_num)
+if __name__ == "__main__":
+    main()
